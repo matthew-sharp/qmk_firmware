03349c7395edc4cabbcbe52ff28808887993426f
Matthew Sharp <>
2018-08-25 20:53:47 +1000

    Fix tap toggle. Add layer RGB. Fix T. Fix To 0 on layer 3.

--- keyboards/xd75/keymaps/sharpy/keymap.c
+++ keyboards/xd75/keymaps/sharpy/keymap.c
@@ -31,9 +31,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_ortho_5x15(
     LSFT(KC_ESC), KC_F3, KC_PLUS, KC_EQL, KC_ASTR, KC_F4, LCTL(LSFT(KC_Z)), _______, LCTL(LSFT(KC_F10)), KC_F5, KC_CIRC, KC_PERC, KC_TILD, KC_F12, _______
    ,_______, LSFT(KC_J), LSFT(KC_H), LSFT(KC_O), LSFT(KC_U), LSFT(KC_K), LSFT(KC_HOME), KC_HASH, LSFT(KC_PGUP), LSFT(KC_G), LSFT(KC_C), LSFT(KC_R), LSFT(KC_F), LSFT(KC_Z), _______
-   ,KC_UNDS, LSFT(KC_Q), LSFT(KC_I), LSFT(KC_E), LSFT(KC_A), LSFT(KC_Y), LSFT(KC_END), KC_DLR, LSFT(KC_PGDN), LSFT(KC_D), LSFT(KC_S), LSFT(KC_F), LSFT(KC_N), LSFT(KC_B), KC_SCLN
+   ,KC_UNDS, LSFT(KC_Q), LSFT(KC_I), LSFT(KC_E), LSFT(KC_A), LSFT(KC_Y), LSFT(KC_END), KC_DLR, LSFT(KC_PGDN), LSFT(KC_D), LSFT(KC_S), LSFT(KC_T), LSFT(KC_N), LSFT(KC_B), KC_SCLN
    ,_______, KC_QUES, KC_EXLM, KC_GRV, KC_AT, LSFT(KC_X), LSFT(KC_ENT), OSM(MOD_LALT|MOD_LSFT), OSL(2), LSFT(KC_W), LSFT(KC_M), LSFT(KC_L), LSFT(KC_P), LSFT(KC_V), _______
    ,OSM(MOD_LCTL|MOD_LSFT), LSFT(KC_LEFT), LSFT(KC_UP), LSFT(KC_DOWN), LSFT(KC_RGHT), LSFT(KC_SPC), LSFT(KC_TAB), OSM(MOD_LCTL|MOD_LSFT), _______, LSFT(KC_BSPC), LSFT(KC_DEL), OSM(MOD_LGUI|MOD_LSFT), KC_MNXT, LSFT(KC_RALT), OSM(MOD_LCTL|MOD_LSFT)),
   [2] = LAYOUT_ortho_5x15(KC_NO, KC_4, KC_0, KC_1, KC_2, KC_3, KC_MUTE, MAGIC_HOST_NKRO, KC_NO, KC_7, KC_6, KC_5, KC_9, KC_8, _______, _______, KC_NO, KC_LT, KC_DLR, KC_GT, KC_NO, KC_VOLD, KC_NO, KC_MPRV, KC_INS, KC_LBRC, KC_AMPR, KC_RBRC, KC_PAUS, _______, KC_MINS, KC_BSLS, KC_LPRN, KC_DQUO, KC_RPRN, KC_HASH, KC_VOLU, KC_NO, KC_MNXT, KC_PERC, KC_LCBR, KC_EQL, KC_RCBR, KC_PIPE, KC_SCLN, KC_LSPO, KC_SLSH, KC_COLN, KC_ASTR, KC_PLUS, KC_NO, _______, _______, _______, KC_PSCR, KC_UNDS, KC_CIRC, KC_TILD, KC_NO, KC_RSPC, _______, _______, _______, _______, _______, _______, _______, _______, KC_NO, _______, _______, KC_APP, KC_NO, KC_NO, _______),
-  [3] = LAYOUT_ortho_5x15(KC_NO, KC_NO, KC_SLSH, KC_ASTR, KC_MINS, KC_NO, KC_NO, RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_COLN, KC_7, KC_8, KC_9, KC_PLUS, KC_CIRC, KC_NO, DEBUG, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, _______, KC_TAB, KC_4, KC_5, KC_6, KC_EQL, KC_COMM, KC_NO, KC_NO, KC_NO, KC_NO, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_ENT, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_0, KC_0, KC_DOT, KC_ENT, KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
+  [3] = LAYOUT_ortho_5x15(KC_NO, KC_NO, KC_SLSH, KC_ASTR, KC_MINS, KC_NO, KC_NO, RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _______, KC_COLN, KC_7, KC_8, KC_9, KC_PLUS, KC_CIRC, KC_NO, DEBUG, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, _______, KC_TAB, KC_4, KC_5, KC_6, KC_EQL, KC_COMM, KC_NO, KC_NO, KC_NO, KC_NO, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_ENT, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_0, KC_0, KC_DOT, KC_ENT, KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
 };
+
+uint32_t layer_state_set_user(uint32_t state) {
+  switch (biton32(state)) {
+  case 0:
+    rgblight_setrgb (0x00,  0x00, 0x00);
+    break;
+  case 1:
+    rgblight_setrgb (0xFF,  0x00, 0x00);
+    break;
+  case 2:
+    rgblight_setrgb (0x00,  0xFF, 0x00);
+    break;
+  case 3:
+    rgblight_setrgb (0x7A,  0x00, 0xFF);
+    break;
+  }
+  return state;
+}
