9e650e639d6a22f3895f365b01f5d066a073f99a
Matthew Sharp <>
2018-08-29 20:31:14 +1000

    Allow Enter, Backspace and Delete from the base layer.

--- keyboards/xd75/keymaps/sharpy/keymap.c
+++ keyboards/xd75/keymaps/sharpy/keymap.c
@@ -72,8 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,   KC_NO,   KC_EQL,  KC_SLSH, KC_ASTR, KC_MINS, KC_NO,   RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,    _______
    ,KC_NO,   KC_COLN, KC_7,    KC_8,    KC_9,    KC_PLUS, KC_NO,   DEBUG,   KC_NO,   KC_NO,   KC_F1,   KC_F2,   KC_F3,  KC_F4,    _______
    ,KC_LPRN, KC_TAB,  KC_4,    KC_5,    KC_6,    KC_PLUS, KC_RPRN, KC_NO,   KC_NO,   KC_NO,   KC_F5,   KC_F6,   KC_F7,  KC_F8,    KC_NO
-   ,_______, KC_COMM, KC_1,    KC_2,    KC_3,    KC_ENT,  KC_NO,   KC_UP,   KC_NO,   KC_NO,   KC_F9,   KC_F10,  KC_F11, KC_F12,   _______
-   ,_______, KC_NO,   KC_0,    KC_0,    KC_DOT,  KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,    _______)
+   ,_______, KC_COMM, KC_1,    KC_2,    KC_3,    KC_ENT,  _______, KC_UP,   KC_NO,   KC_NO,   KC_F9,   KC_F10,  KC_F11, KC_F12,   _______
+   ,_______, KC_NO,   KC_0,    KC_0,    KC_DOT,  KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_NO,   KC_NO,  KC_NO,    _______)
 };
 
 uint32_t layer_state_set_user(uint32_t state) {
