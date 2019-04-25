23c512b59d556b7c07e885d5747ae81a49af8339
Matthew Sharp <>
2019-04-25 12:18:12 +1000

    Add layer names. Make symbols closer to ergodox

--- keyboards/xd75/keymaps/sharpy/keymap.c
+++ keyboards/xd75/keymaps/sharpy/keymap.c
@@ -3,6 +3,13 @@
 // Helpful defines
 #define _______ KC_TRNS
 
+// Layer names
+#define LWBKL 0
+#define LWSBKL 1
+#define SYM 2
+#define NUM 3
+#define CTRLSH 4
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Layer 0+1: Base+Shift
  * .--------------------------------------------------------------------------------------------------------------------------------------.
@@ -22,17 +29,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  OSL4  |  Left  |   Up   |  Down  |  Right |  Spc   |  OSL1  |  Ctrl  |  Tab   |  Bksp  |  Del   |  Shift | Play/Ps| AltGr  |  Ctrl  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
-  [0] = LAYOUT_ortho_5x15(
-    KC_PIPE, KC_4,    KC_0,    KC_1,    KC_2,    KC_3,    LCTL(KC_Z), TT(1), LCTL(KC_F10), KC_7, KC_6, KC_5,    KC_9,   KC_8,     TO(0)
-   ,TT(2),   KC_J,    KC_H,    KC_O,    KC_U,    KC_K,    KC_HOME, KC_DQUO, KC_PGUP, KC_G,    KC_C,    KC_R,    KC_F,   KC_Z,     TT(3)
+  [LWBKL] = LAYOUT_ortho_5x15(
+    KC_PIPE, KC_4,    KC_0,    KC_1,    KC_2,    KC_3,    LCTL(KC_Z), TT(LWSBKL), LCTL(KC_F10), KC_7, KC_6, KC_5, KC_9, KC_8,     TO(LWBKL)
+   ,TT(SYM), KC_J,    KC_H,    KC_O,    KC_U,    KC_K,    KC_HOME, KC_DQUO, KC_PGUP, KC_G,    KC_C,    KC_R,    KC_F,   KC_Z,     TT(NUM)
    ,KC_MINS, KC_Q,    KC_I,    KC_E,    KC_A,    KC_Y,    KC_END, KC_AMPR,  KC_PGDN, KC_D,    KC_S,    KC_T,    KC_N,   KC_B,     KC_COLN
-   ,LT(1,KC_ESCAPE), KC_SLSH, KC_COMM, KC_QUOT, KC_DOT, KC_X, KC_ENT, OSM(MOD_LALT), OSL(2), KC_W, KC_M, KC_L,  KC_P,   KC_V,     LT(1,KC_ESCAPE)
-   ,OSL(4), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_SPC, OSL(1), OSM(MOD_LCTL), KC_TAB, KC_BSPC, KC_DEL, OSM(MOD_LSFT), KC_MPLY, KC_RALT, OSM(MOD_RCTL)),
-  [1] = LAYOUT_ortho_5x15(
+   ,LT(LWSBKL,KC_ESCAPE), KC_SLSH, KC_COMM, KC_QUOT, KC_DOT, KC_X, KC_ENT, OSM(MOD_LALT), OSL(SYM), KC_W, KC_M, KC_L, KC_P, KC_V, LT(LWSBKL,KC_ESCAPE)
+   ,OSL(CTRLSH), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_SPC, OSL(LWSBKL), OSM(MOD_LCTL), KC_TAB, KC_BSPC, KC_DEL, OSM(MOD_LSFT), KC_MPLY, KC_RALT, OSM(MOD_RCTL)),
+  [LWSBKL] = LAYOUT_ortho_5x15(
     LSFT(KC_ESC), KC_F3, KC_PLUS, KC_EQL, KC_ASTR, KC_F4, LCTL(LSFT(KC_Z)), _______, LCTL(LSFT(KC_F10)), KC_F5, KC_CIRC, KC_PERC, KC_TILD, KC_F12, _______
    ,_______, LSFT(KC_J), LSFT(KC_H), LSFT(KC_O), LSFT(KC_U), LSFT(KC_K), LSFT(KC_HOME), KC_HASH, LSFT(KC_PGUP), LSFT(KC_G), LSFT(KC_C), LSFT(KC_R), LSFT(KC_F), LSFT(KC_Z), _______
    ,KC_UNDS, LSFT(KC_Q), LSFT(KC_I), LSFT(KC_E), LSFT(KC_A), LSFT(KC_Y), LSFT(KC_END), KC_DLR, LSFT(KC_PGDN), LSFT(KC_D), LSFT(KC_S), LSFT(KC_T), LSFT(KC_N), LSFT(KC_B), KC_SCLN
-   ,_______, KC_QUES, KC_EXLM, KC_GRV, KC_AT, LSFT(KC_X), LSFT(KC_ENT), OSM(MOD_LALT|MOD_LSFT), OSL(2), LSFT(KC_W), LSFT(KC_M), LSFT(KC_L), LSFT(KC_P), LSFT(KC_V), _______
+   ,_______, KC_QUES, KC_EXLM, KC_GRV, KC_AT, LSFT(KC_X), LSFT(KC_ENT), OSM(MOD_LALT|MOD_LSFT), OSL(SYM), LSFT(KC_W), LSFT(KC_M), LSFT(KC_L), LSFT(KC_P), LSFT(KC_V), _______
    ,OSM(MOD_LCTL|MOD_LSFT), S(KC_LEFT), S(KC_UP), S(KC_DOWN), S(KC_RGHT), S(KC_SPC), _______, OSM(MOD_LCTL|MOD_LSFT), S(KC_TAB), S(KC_BSPC), S(KC_DEL), OSM(MOD_LGUI), KC_MNXT, S(KC_RALT), OSM(MOD_LCTL|MOD_LSFT)),
 
 /* Layer 2: Symbols
@@ -41,18 +48,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        |  Calc  |   <    |   $    |   >    |        |  Vol-  |        |  Prev  | Insert |   [    |   &    |   ]    | Pause  |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |   -    |   \    |   (    |   "    |   )    |   #    |  Vol+  |        |  Next  |   %    |   {    |   =    |   }    |   |    |   ;    |
+ * |   -    |   \    |   (    |   "    |   )    |   +    |  Vol+  |        |  Next  |   %    |   {    |   =    |   }    |   ~    |   ;    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | Shft/( |   /    |   :    |   *    |   +    |        |        |        |        | PrntSc |   _    |   ^    |   ~    |        | Shft/) |
+ * | Shft/( |   /    |   :    |   *    |   #    |        |        |        |        | PrntSc |   _    |   ^    |   |    |        | Shft/) |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        |        |        |        |        |        |        |        |        |        |        |  App   |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
-  [2] = LAYOUT_ortho_5x15(
+  [SYM] = LAYOUT_ortho_5x15(
     KC_NO,   KC_4,    KC_0,    KC_1,    KC_2,    KC_3,    KC_MUTE, MAGIC_HOST_NKRO, KC_NO, KC_7, KC_6, KC_5,    KC_9,   KC_8,     _______
    ,_______, KC_CALC, KC_LT,   KC_DLR,  KC_GT,   KC_NO,   KC_VOLD, _______, KC_MPRV, KC_INS,  KC_LBRC, KC_AMPR, KC_RBRC,KC_PAUS,  _______
-   ,KC_MINS, KC_BSLS, KC_LPRN, KC_DQUO, KC_RPRN, KC_HASH, KC_VOLU, _______, KC_MNXT, KC_PERC, KC_LCBR, KC_EQL,  KC_RCBR,KC_PIPE,  KC_SCLN
-   ,KC_LSPO, KC_SLSH, KC_COLN, KC_ASTR, KC_PLUS, KC_NO,   _______, _______, _______, KC_PSCR, KC_UNDS, KC_CIRC, KC_TILD,KC_NO,    KC_RSPC
+   ,KC_MINS, KC_BSLS, KC_LPRN, KC_DQUO, KC_RPRN, KC_PLUS, KC_VOLU, _______, KC_MNXT, KC_PERC, KC_LCBR, KC_EQL,  KC_RCBR,KC_TILD,  KC_SCLN
+   ,KC_LSPO, KC_SLSH, KC_COLN, KC_ASTR, KC_HASH, KC_NO,   _______, _______, _______, KC_PSCR, KC_UNDS, KC_CIRC, KC_PIPE,KC_NO,    KC_RSPC
    ,_______, _______, _______, _______, _______, _______, _______, _______, KC_NO,   _______, _______, KC_APP,  KC_NO,  _______,  _______),
 
 /* Layer 3: Numpad and function
@@ -68,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |        |   0    |   0    |   .    |  Spc   |  Left  |  Down  | Right  |        |        |        |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
-  [3] = LAYOUT_ortho_5x15(
+  [NUM] = LAYOUT_ortho_5x15(
     KC_NO,   KC_NO,   KC_EQL,  KC_SLSH, KC_ASTR, KC_MINS, KC_NO,   RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,    _______
    ,KC_NO,   KC_COLN, KC_7,    KC_8,    KC_9,    KC_PLUS, KC_NO,   DEBUG,   KC_NO,   KC_NO,   KC_F1,   KC_F2,   KC_F3,  KC_F4,    _______
    ,KC_LPRN, KC_TAB,  KC_4,    KC_5,    KC_6,    KC_PLUS, KC_RPRN, KC_NO,   KC_NO,   KC_NO,   KC_F5,   KC_F6,   KC_F7,  KC_F8,    KC_NO
@@ -88,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
-  [4] = LAYOUT_ortho_5x15(
+  [CTRLSH] = LAYOUT_ortho_5x15(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
    ,_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
    ,_______, LCTL(KC_A),_______,_______,_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
